6420240bf8ec8518903eb1dd7bd2e2d740643b0a
Berkan Barış Tezcan <berkan.baris.tezcan@gmail.com>
2020-08-08 00:48:08 +0300

    Added parallax background

---
+++ Assets/Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0.0f);
+        cam.transform.position = new Vector3(cam.transform.position.x + 3 * move.x * Time.deltaTime, cam.transform.position.y, cam.transform.position.z);
+    }
+}
