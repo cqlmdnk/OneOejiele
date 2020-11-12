84a41ba03d7ca49151f43a0a72c061d4f9a90331
cqlmdnk <berkan.baris.tezcan@gmail.com>
2020-11-12 18:08:30 +0300

    Container auto-deletion script

---
+++ Assets/ContainerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int  removedChildCount = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.childCount != removedChildCount)
+        {
+            removedChildCount = 0;
+            for (int i = 0; i < this.transform.childCount; i++)
+            {
+                Destroy(this.transform.GetChild(i).gameObject, 10.0f);
+                removedChildCount++;
+            }
+        }
+       
+        
+
+    }
+}
