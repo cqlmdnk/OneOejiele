4e34c7542543c01588a05a600b6f9bec6a6e2750
cqlmdnk <berkan.baris.tezcan@gmail.com>
2020-11-23 19:42:04 +0300

    Arm positioning when character is flipped

--- Assets/ArcherChar/ArmControl.cs
+++ Assets/ArcherChar/ArmControl.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ArmControl : MonoBehaviour
 {
     // Start is called before the first frame update
+    private bool movedLeft  = false;
     void Start()
     {
         
@@ -17,6 +18,17 @@ public class ArmControl : MonoBehaviour
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float angle = (float)Math.Atan2(mousePos.y- transform.position.y, mousePos.x- transform.position.x+0.5f)* Mathf.Rad2Deg ;
+        
+        if((angle > 90.0f || angle <-90.0f) && !movedLeft)
+        {
+            transform.position = new Vector3(transform.position.x-0.15f, transform.position.y, transform.position.z);
+            movedLeft = true;
+        }
+        else if (!(angle > 90.0f || angle < -90.0f) && movedLeft)
+        {
+            transform.position = new Vector3(transform.position.x +0.15f, transform.position.y, transform.position.z);
+            movedLeft = false;
+        }
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         if (transform.parent.gameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("character_run"))
         {
